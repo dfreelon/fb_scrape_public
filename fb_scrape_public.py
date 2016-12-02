12feb5730b41f6964730ae2d10687c9fa4ae5320
Deen Freelon <dfreelon@gmail.com>
2016-12-01 23:11:16 -0500

    Fixed bug in loading routine

--- fb_scrape_public.py
+++ fb_scrape_public.py
@@ -5,7 +5,7 @@ INSTRUCTIONS
 
 1.    This script is written for Python 3 and won't work with previous Python versions.
 2.    The main function in this module is scrape_fb (see lines 107-109). It is the only function most users will need to run directly.
-3.    You need to create your own Facebook app, which you can do here: https://developers.facebook.com/apps . Doesn't matter what you call it, you just need to pull the unique client ID (app ID) and app secret for your new app.
+3.    You need to create your own Facebook app, which you can do here: https://developers.facebook.com/apps . Doesn't matter what you call your new app, you just need to pull its unique client ID (app ID) and app secret.
 4.    Once you create your app, you can insert the client ID and secret AS STRINGS into the appropriate scrape_fb fields. 
 5.    This function accepts text FB user IDs ('barackobama'), numerical user IDs, and post IDs. You can load them into the ids field using a comma-delimited string or by creating a plain text file in the same folder as the script containing one or more names of the Facebook pages you want to scrape, one ID per line. For example, if you wanted to scrape Barack Obama's official FB page (http://facebook.com/barackobama/) using the text file method, your first line would simply be 'barackobama' without quotes. I suggest starting with only one ID to make sure it works. You'll only be able to collect data from public pages.
 6.    The only required fields for the scrape_fb function are client_id, client_secret, and ids. I recommend not changing the other defaults (except for maybe outfile) unless you know what you're doing.
@@ -113,7 +113,7 @@ def scrape_fb(client_id,client_secret,ids,outfile="fb_data.csv",version="2.7",sc
     time1 = time.time()
     fb_urlobj = urllib.request.urlopen('https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret)
     fb_token = fb_urlobj.read().decode(encoding="latin1")
-    if type(ids) == 'str':
+    if "." in ids:
         fb_ids = [i[0].strip() for i in load_data(ids)]
     else:
         fb_ids = [i.strip() for i in ids.split(",")]
