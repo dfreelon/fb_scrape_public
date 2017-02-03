8691e116e964b70c5f1c8ffb8e3c699585c48a03
Deen Freelon <dfreelon@gmail.com>
2017-02-03 09:24:32 -0500

    Bugfix!
    Now accepts single ids in string format.

--- fb_scrape_public.py
+++ fb_scrape_public.py
@@ -1,5 +1,5 @@
 '''
-This script can download posts and comments from public Facebook pages. It requires Python 3.
+This script can download posts and comments from public Facebook pages and groups. It requires Python 3.
 
 INSTRUCTIONS
 
@@ -7,7 +7,7 @@ INSTRUCTIONS
 2.    The main function in this module is scrape_fb (see lines 107-109). It is the only function most users will need to run directly.
 3.    You need to create your own Facebook app, which you can do here: https://developers.facebook.com/apps . Doesn't matter what you call your new app, you just need to pull its unique client ID (app ID) and app secret.
 4.    Once you create your app, you can insert the client ID and secret AS STRINGS into the appropriate scrape_fb fields. 
-5.    This function accepts text FB user IDs ('barackobama'), numerical user IDs, and post IDs. You can load them into the ids field using a comma-delimited string or by creating a plain text file in the same folder as the script containing one or more names of the Facebook pages you want to scrape, one ID per line. For example, if you wanted to scrape Barack Obama's official FB page (http://facebook.com/barackobama/) using the text file method, your first line would simply be 'barackobama' without quotes. I suggest starting with only one ID to make sure it works. You'll only be able to collect data from public pages.
+5.    This function accepts text FB user IDs ('barackobama'), numerical user IDs, and post IDs. You can load them into the ids field using a comma-delimited string or by creating a plain text file in the same folder as the script containing one or more names of the Facebook pages you want to scrape, one ID per line (this file MUST have a .csv or .txt extension). For example, if you wanted to scrape Barack Obama's official FB page (http://facebook.com/barackobama/) using the text file method, your first line would simply be 'barackobama' without quotes. I suggest starting with only one ID to make sure it works. You'll only be able to collect data from public pages.
 6.    The only required fields for the scrape_fb function are client_id, client_secret, and ids. I recommend not changing the other defaults (except for maybe outfile) unless you know what you're doing.
 7.    If you did everything correctly, the command line should show you some informative status messages. Eventually it will save a CSV full of data to the same folder where this script was run. If something went wrong, you'll see an error.
 '''
@@ -111,12 +111,16 @@ def make_csv_chunk(fb_json_page,scrape_mode,thread_starter='',msg=''):
 
 def scrape_fb(client_id,client_secret,ids,outfile="fb_data.csv",version="2.7",scrape_mode="feed",end_date=""):
     time1 = time.time()
+    if type(client_id) is int:
+        client_id = str(client_id)
     fb_urlobj = urllib.request.urlopen('https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret)
     fb_token = fb_urlobj.read().decode(encoding="latin1")
-    if "." in ids:
+    if "," in ids:
+        fb_ids = [i.strip() for i in ids.split(",")]
+    elif '.csv' in ids or '.txt' in ids:
         fb_ids = [i[0].strip() for i in load_data(ids)]
     else:
-        fb_ids = [i.strip() for i in ids.split(",")]
+        fb_ids = [ids]
 
     try:
         end_dateobj = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
@@ -202,3 +206,4 @@ def scrape_fb(client_id,client_secret,ids,outfile="fb_data.csv",version="2.7",sc
 
     print('Script completed in',time.time()-time1,'seconds.')
     return csv_data
+    
